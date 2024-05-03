22a7b150fd37506a763be3e3ecf8307e4ce58229
Oldsmob1le <abdulin00@vk.com>
2024-05-03 23:35:51 +0300

    account end without adaptive

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -5,21 +5,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
     const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
 
-    // Функция для переключения с входа на регистрацию
     switchToRegisterFromLogin.addEventListener('click', function (e) {
         e.preventDefault();
-        loginModal.hide();  // Закрываем окно входа
-        registerModal.show();  // Открываем окно регистрации
+        loginModal.hide();  
+        registerModal.show();
     });
 
-    // Функция для переключения с регистрации на вход
     switchToLoginFromRegister.addEventListener('click', function (e) {
         e.preventDefault();
-        registerModal.hide();  // Закрываем окно регистрации
-        loginModal.show();  // Открываем окно входа
+        registerModal.hide(); 
+        loginModal.show(); 
     });
 
-    // Код для корректного закрытия модальных окон при переключении
     $('#loginModal').on('hidden.bs.modal', function () {
         registerModal.hide();
     });
--- assets/script/payment.js
+++ assets/script/payment.js
@@ -1,16 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Получите все элементы с классом "payment-item"
     const paymentItems = document.querySelectorAll('.payment-item');
 
-    // Функция для добавления класса при нажатии на элемент
     function toggleBackground(event) {
         const element = event.currentTarget;
 
-        // Переключить класс "active" для изменения стилей
         element.classList.toggle('active');
     }
-
-    // Добавьте обработчик событий для каждого элемента
     paymentItems.forEach(function(item) {
         item.addEventListener('click', toggleBackground);
     });
